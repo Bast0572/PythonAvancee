e30820e29db090a52c656fa448fff56ae236cbed
Bast0572 <bastien.gaudin@angers.globalopencampus.com>
2023-11-22 17:09:28 +0100

    substitution

--- cesar.py
+++ cesar.py
@@ -9,3 +9,16 @@ def cesar(chaine,valeur):
     return print(chaine2)
 
 cesar("Bonjour le monde",3)
+
+
+def cesarInverse(chaine, valeur):
+    char = ''
+    chaine2 = ""
+    for item in chaine:
+        if item == ' ':
+            chaine2 = chaine2+item
+        else:
+            chaine2 = chaine2+(chr(ord(item)-valeur))
+    return print(chaine2)
+
+cesarInverse("Erqmrxu oh prqgh", 3)
