5522c3444a688e8e0cc3e45cacd4320190eccfc6
Bast0572 <bastien.gaudin@angers.globalopencampus.com>
2023-11-22 14:57:04 +0100

    maj binaire + recherche naive

--- recherchebinaire.py
+++ recherchebinaire.py
@@ -1,8 +1,18 @@
-def recherche_binaire(liste,param):
-    for i in range(len(liste)-1):
-        if liste[i]==param:
-            return print(i)
-    
+def recherche_binaire(liste, param):
+    debut = 0
+    fin = len(liste)-1
+
+    while debut <= fin:
+      milieu = (debut+fin)//2
+      if liste[milieu] == param:
+            return print(milieu)
+      else:
+           if liste[milieu] > param:
+                fin = fin-1
+           else:
+                debut = debut+1
+
     return print('-1')
 
-recherche_binaire([1,2,3,4,5,7,8,9],6)
+recherche_binaire([1,2,3,4,5,7,8,9],5)
+
