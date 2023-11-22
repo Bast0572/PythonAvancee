251daedec1d24bb3180488f8afce2dcfe142e016
Bast0572 <bastien.gaudin@angers.globalopencampus.com>
2023-11-22 17:06:13 +0100

    cesar

---
+++ cesar.py
@@ -0,0 +1,11 @@
+def cesar(chaine,valeur):
+    char=''
+    chaine2=""
+    for item in chaine:
+        if item==' ':
+            chaine2=chaine2+item
+        else:
+            chaine2=chaine2+(chr(ord(item)+valeur))
+    return print(chaine2)
+
+cesar("Bonjour le monde",3)
